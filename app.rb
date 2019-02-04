a01458ee96ddf518c3b32ed00a5415a66ef55a53
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-02-04 17:09:40 -0600

    navigating variables structure bewteen erb and ruby

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get "/index" do 
-	erb :index
+get "/" do 
+	userword2  = params["userword1"]
+	usercode2  = params["usercode1"]
+  erb :index, :locals => {:userword => userword2, :usercode => usercode2}
 end
+
