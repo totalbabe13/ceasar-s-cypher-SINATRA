c92985f24c127776c6aecb52245d3ad2f7a7dcdc
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-02-04 19:26:25 -0600

    finished

--- app.rb
+++ app.rb
@@ -3,7 +3,60 @@ require "sinatra/reloader"
 
 get "/" do 
 	userword2  = params["userword1"]
-	usercode2  = params["usercode1"]
-  erb :index, :locals => {:userword => userword2, :usercode => usercode2}
+	usercode2  = params["usercode1"].to_i
+	testing = caesar_cipher(userword2.to_s,usercode2)
+  erb :index, :locals => {:userword => userword2, :usercode => usercode2, :testing => testing}
 end
 
+def caesar_cipher(phrase,num)
+  original_phrase_array = phrase.split('')
+  downcased_phrase = phrase.downcase
+  alphabet = ("a".."z").to_a
+  numbers = (1..26).to_a
+  #convert phrase to array
+  phrase_array = downcased_phrase.split('')
+  #convert phrase_array to numbers 
+  number_code = []
+  x = 0
+  while x != phrase_array.length
+    if alphabet.index(phrase_array[x])
+      number_code << (alphabet.index(phrase_array[x])) + 1 
+      x += 1
+    else
+      number_code << phrase_array[x]
+      x += 1
+    end   
+  end
+  #use numbers with cypher to find new letter index 
+  find_new_letter_index = []
+  number_code.map do |item| 
+    if item.is_a?(Integer)
+      find_new_letter_index << item + num
+    else
+      find_new_letter_index << item   
+    end  
+  end
+  # match new letter index with letters
+  secret_message_array = []
+  find_new_letter_index.each do |item|
+     if item.is_a?(Integer)
+       if item > 26
+         item = item - 26
+       end   
+       secret_message_array << alphabet[item-1]
+     else
+       secret_message_array << item
+     end     
+   end
+  uppercased_array = []
+  original_phrase_array.each do |letter|
+    if letter == letter.upcase
+      k = original_phrase_array.index(letter)
+      secret_message_array[k] = secret_message_array[k].upcase
+    else
+      letter  
+    end
+  end
+  
+  secret_message_array.join
+end
